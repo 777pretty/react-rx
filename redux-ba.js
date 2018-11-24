5cf3b28fd4ccc30815f8d2c245a5b2c1c6e2f51d
777z <7b.onus583@gmail.com>
2018-11-24 23:06:24 +0100

    Test action dispatching

--- redux-ba.js
+++ redux-ba.js
@@ -7,6 +7,19 @@ const initialState = {
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC_COUNT'){
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+
+    if (action.type === 'ADD_COUNT'){
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
     return state;
 };
 
@@ -15,5 +28,8 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Dispatching Action
+store.dispatch({type: 'INC_COUNT'});
+store.dispatch({type: 'ADD_COUNT', value: 80});
+console.log(store.getState());
 
 // Subscription
